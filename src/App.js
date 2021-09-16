3f1b67d9c81ad70f6f9eaf1043f038fa556eb39d
afanfian <afanfian14@gmail.com>
2021-09-16 10:27:09 +0700

    Belajar React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./asset/Header";
+import From from "./asset/From";
+import TodosList from "./asset/TodosList";
+const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <From input={input} setInput={setInput} todos={todos} setTodos={setTodos} />
+        </div>
+        <div>
+          <TodosList todos={todos} setTodos={setTodos} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
